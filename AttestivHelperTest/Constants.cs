9d684431d87fa92927e6b6ced627a7c71ae7922b
Joseph Joy <joseph.joy@tungstenautomation.com>
2024-12-03 19:20:20 +1100

    Code refactor and cleanup

---
+++ AttestivHelperTest/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace tungstenlabs.integration.attestiv.tests
+{
+    public static class Constants
+    {
+        public const string TOTALAGILITY_API_URL = @"https://ktacloudeco-dev.ktaprt.dev.kofaxcloud.com/Services/Sdk";
+        public const string TOTALAGILITY_SESSION_ID = @"TO-BE-UPDATED-WHEN-TESTING";
+
+        public const string ATTESTIV_API_URL = @"TO-BE-UPDATED-WHEN-TESTING";
+        public const string ATTESTIV_USERNAME = @"TO-BE-UPDATED-WHEN-TESTING";
+        public const string ATTESTIV_PASSWORD = @"TO-BE-UPDATED-WHEN-TESTING";
+    }
+}
--- AttestivHelperTest/Properties/AssemblyInfo.cs
+++ AttestivHelperTest/Properties/AssemblyInfo.cs
@@ -1,18 +1,15 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AttestivHelperTest")]
+[assembly: AssemblyTitle("tungstenlabs.integration.attestiv.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AttestivHelperTest")]
+[assembly: AssemblyProduct("tungstenlabs.integration.attestiv.tests")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("a1dc1075-9bbd-44b5-a10f-cc71d3512557")]
 
 // [assembly: AssemblyVersion("1.0.*")]
